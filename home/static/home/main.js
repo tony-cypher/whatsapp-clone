a11bc21c2ade47da35bd7bb6546f226620812f95
Cipher <tonycypher0@gmail.com>
2024-08-17 12:21:17 +0100

    completed template adjustment

--- home/static/home/main.js
+++ home/static/home/main.js
@@ -1,29 +1,23 @@
-const room = JSON.parse(document.getElementById("jroom").textContent);
-if (room == "public") {
-  public_group();
-} else if (room == "private") {
-  private_chat();
-}
+// --------------------------    Public chat    ---------------------//
 
-function public_group() {
-  const roomName = JSON.parse(
-    document.getElementById("json-roomname").textContent
-  );
-  const username = JSON.parse(
-    document.getElementById("json-username").textContent
-  );
+const roomName = JSON.parse(
+  document.getElementById("json-roomname").textContent
+);
+const username = JSON.parse(
+  document.getElementById("json-username").textContent
+);
 
-  const chatSocket = new WebSocket(
-    "ws://" + window.location.host + "/ws/chatroom/" + roomName + "/"
-  );
+const chatSocket = new WebSocket(
+  "ws://" + window.location.host + "/ws/chatroom/" + roomName + "/"
+);
 
-  chatSocket.onmessage = function (e) {
-    console.log("on message");
-    const data = JSON.parse(e.data);
-    console.log(data);
-    console.log(data.message);
-    if (data.message) {
-      let html = `<div>
+chatSocket.onmessage = function (e) {
+  console.log("on message");
+  const data = JSON.parse(e.data);
+  console.log(data);
+  console.log(data.message);
+  if (data.message) {
+    let html = `<div>
         <div class="w-50 ms-auto">
           <div class="bg-success text-white rounded">
             <p id="chat" class="p-1">${data.message}</p>
@@ -31,63 +25,60 @@ function public_group() {
         </div>
       </div>`;
 
-      $("#chat-messages").append(html);
-      scrollToBottom();
-    }
+    $("#chat-messages").append(html);
+    scrollToBottom();
+  }
 
-    if (data.user_count !== undefined) {
-      console.log("user_count", data.user_count);
-      document.getElementById(
-        "userCount"
-      ).innerHTML = `<i class="bi bi-dot text-success"></i>${data.user_count} online`;
-    }
-  };
+  if (data.user_count !== undefined) {
+    console.log("user_count", data.user_count);
+    document.getElementById(
+      "userCount"
+    ).innerHTML = `<i class="bi bi-dot text-success"></i>${data.user_count} online`;
+  }
+};
 
-  chatSocket.onclose = function (e) {
-    console.error("Socket closed");
-  };
-  // When the send button is clicked
-  // collects the message in the form
-  document.getElementById("send").onclick = function (e) {
-    // e.preventDefault() stops the server from reloading when message is sent.
-    e.preventDefault();
-    const messageinput = document.getElementById("message-input");
-    const message = messageinput.value;
+chatSocket.onclose = function (e) {
+  console.error("Socket closed");
+};
+// When the send button is clicked
+// collects the message in the form
+document.getElementById("send").onclick = function (e) {
+  // e.preventDefault() stops the server from reloading when message is sent.
+  e.preventDefault();
+  const messageinput = document.getElementById("message-input");
+  const message = messageinput.value;
 
-    // compiles the output as a dictionary and sends the output to consumers.py using chatSocket
-    chatSocket.send(
-      JSON.stringify({
-        message: message,
-        username: username,
-        room: roomName,
-      })
-    );
-
-    // resets messageinput.value to '' (empty) after sending to consumers.py
-    messageinput.value = "";
-  };
-}
-
-function private_chat() {
-  const user1_id = JSON.parse(document.getElementById("user1_id").textContent);
-  const user2_id = JSON.parse(document.getElementById("user2_id").textContent);
-  const username = JSON.parse(
-    document.getElementById("json-username").textContent
+  // compiles the output as a dictionary and sends the output to consumers.py using chatSocket
+  chatSocket.send(
+    JSON.stringify({
+      message: message,
+      username: username,
+      room: roomName,
+    })
   );
 
-  const privateRoomName = `private_${Math.min(user1_id, user2_id)}_${Math.max(
-    user1_id,
-    user2_id
-  )}`;
-  console.log(privateRoomName);
-  const privateChatSocket = new WebSocket(
-    "ws://" + window.location.host + "/ws/private_chat/" + privateRoomName + "/"
-  );
-  privateChatSocket.onmessage = function (e) {
-    console.log("on message");
-    const data = JSON.parse(e.data);
-    if (data.message) {
-      let html = `<div>
+  // resets messageinput.value to '' (empty) after sending to consumers.py
+  messageinput.value = "";
+};
+
+// ----------------------    Private Chat    ------------------------//
+
+const user1_id = JSON.parse(document.getElementById("user1_id").textContent);
+const user2_id = JSON.parse(document.getElementById("user2_id").textContent);
+
+const privateRoomName = `private_${Math.min(user1_id, user2_id)}_${Math.max(
+  user1_id,
+  user2_id
+)}`;
+console.log(privateRoomName);
+const privateChatSocket = new WebSocket(
+  "ws://" + window.location.host + "/ws/private_chat/" + privateRoomName + "/"
+);
+privateChatSocket.onmessage = function (e) {
+  console.log("on message");
+  const data = JSON.parse(e.data);
+  if (data.message) {
+    let html = `<div>
       <div class="w-50 ms-auto">
         <div class="bg-success text-white rounded">
           <p id="chat" class="p-1">${data.message}</p>
@@ -95,33 +86,32 @@ function private_chat() {
       </div>
     </div>`;
 
-      $("#chat-messages").append(html);
-      scrollToBottom();
-    }
-  };
+    $("#chat-messages").append(html);
+    scrollToBottom();
+  }
+};
 
-  privateChatSocket.onclose = function (e) {
-    console.error("Private chat socket closed");
-  };
+privateChatSocket.onclose = function (e) {
+  console.error("Private chat socket closed");
+};
 
-  document.getElementById("privateSend").onclick = function (e) {
-    // e.preventDefault() stops the server from reloading when message is sent.
-    e.preventDefault();
-    const messageinput = document.getElementById("message-input");
-    const message = messageinput.value;
+document.getElementById("privateSend").onclick = function (e) {
+  // e.preventDefault() stops the server from reloading when message is sent.
+  e.preventDefault();
+  const messageinput = document.getElementById("message-input");
+  const message = messageinput.value;
 
-    // compiles the output as a dictionary and sends the output to consumers.py using chatSocket
-    privateChatSocket.send(
-      JSON.stringify({
-        message: message,
-        author: username,
-      })
-    );
+  // compiles the output as a dictionary and sends the output to consumers.py using chatSocket
+  privateChatSocket.send(
+    JSON.stringify({
+      message: message,
+      author: username,
+    })
+  );
 
-    // resets messageinput.value to '' (empty) after sending to consumers.py
-    messageinput.value = "";
-  };
-}
+  // resets messageinput.value to '' (empty) after sending to consumers.py
+  messageinput.value = "";
+};
 
 function scrollToBottom() {
   const mcontainer = document.getElementById("chat-messages");
--- home/views.py
+++ home/views.py
@@ -12,13 +12,18 @@ def groups(request):
 
 @login_required
 def group_chat(request, name):
+    rooms = ChatGroup.objects.all()
+    chatrooms = PrivateChat.objects.filter(id=request.user.id)
     group = get_object_or_404(ChatGroup, group_name=name)
-    print(group)
     messages = GroupMessage.objects.filter(group=group)
-    return render(request, 'home/group_chat.html', {'group': group, 'messages':messages})
+    data = {'group': group, 'messages':messages, 'rooms': rooms, 'chatrooms': chatrooms}
+    return render(request, 'home/group_chat.html', data)
 
 @login_required
 def private_chat(request, user1, user2):
+    rooms = ChatGroup.objects.all()
+    chatrooms = PrivateChat.objects.filter(id=request.user.id)
     chat = get_object_or_404(PrivateChat, user1=user1, user2=user2)
     messages = PrivateMessage.objects.filter(chat=chat)
-    return render(request, 'home/private_chat.html', {'chat':chat, 'messages':messages})
+    data = {'chat':chat, 'messages':messages, 'rooms': rooms, 'chatrooms': chatrooms}
+    return render(request, 'home/private_chat.html', data)
